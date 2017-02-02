8fa310e4222711af44632822bf264a2769b3a420
phi161 <phi161@users.noreply.github.com>
2017-02-02 20:58:13 +0200

    Add a struct that will hold data from the server

---
+++ Ndebele/Rate.swift
@@ -0,0 +1,17 @@
+//
+//  Rate.swift
+//  Ndebele
+//
+//  Created by phi on 02/02/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import Foundation
+
+struct Rate {
+    let currencyId: Int
+    let name: String
+    let buyPrice: Double
+    let sellPrice: Double
+    let pipMultiplier: Int
+}
