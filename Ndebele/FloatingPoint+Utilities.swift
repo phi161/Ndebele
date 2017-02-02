91a54b938ce6aaac72aca8a2b85b15a1c3ddcc7f
phi161 <phi161@users.noreply.github.com>
2017-02-02 23:53:04 +0200

    Add an extension to FloatingPoint in order to help with String conversions

---
+++ Ndebele/FloatingPoint+Utilities.swift
@@ -0,0 +1,20 @@
+//
+//  FloatingPoint+Utilities.swift
+//  Ndebele
+//
+//  Created by phi on 02/02/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Helps with String conversion
+ Taken from http://stackoverflow.com/a/27705739/289501
+ */
+extension FloatingPoint {
+    func fixedFraction(digits: Int) -> String {
+        //TODO: Better understand what are the implications of using that
+        return String(format: "%.\(digits)f", self as! CVarArg)
+    }
+}
