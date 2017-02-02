95bb71a9280cb541a9cc8d1ff65dab628aa330ca
phi161 <phi161@users.noreply.github.com>
2017-02-02 20:22:34 +0200

    Add initial project files (View-based template)

---
+++ Ndebele/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Ndebele
+//
+//  Created by phi on 02/02/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Ndebele/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Ndebele
+//
+//  Created by phi on 02/02/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
