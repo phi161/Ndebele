d058048f74d52b7ff6a60e252aadb3eaa9f47708
phi161 <phi161@users.noreply.github.com>
2017-02-02 20:46:58 +0200

    Add a cell that will hold information about rates

---
+++ Ndebele/RateTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  RateTableViewCell.swift
+//  Ndebele
+//
+//  Created by phi on 02/02/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import UIKit
+
+class RateTableViewCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var buyLabel: UILabel!
+    @IBOutlet var sellLabel: UILabel!
+    @IBOutlet var spreadLabel: UILabel!
+}
+
+extension RateTableViewCell {
+    func populate(name: String, buyPrice: Double, sellPrice: Double, spread: Double) {
+        nameLabel.text = name
+        buyLabel.text = String(buyPrice)
+        sellLabel.text = String(sellPrice)
+        spreadLabel.text = String(spread)
+    }
+}
