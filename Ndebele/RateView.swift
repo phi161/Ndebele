1050568bdf780f9e9b3d0a97dc0bb6cf332ad570
phi161 <phi161@users.noreply.github.com>
2017-02-08 11:27:48 +0200

    Use an enum for representing the different rate states

--- Ndebele/RateView.swift
+++ Ndebele/RateView.swift
@@ -9,31 +9,48 @@
 import Foundation
 import UIKit
 
-class RateView: UIView, NibOwnerLoadable {
+enum HistoryState {
+    case increased
+    case decreased
+    case unaffected
+}
 
+class RateView: UIView, NibOwnerLoadable {
+    
     @IBOutlet private var label: UILabel!
     @IBOutlet private var roundedView: UIView!
-
+    
     // MARK: - Setup
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         RateView.loadFromNib(owner: self)
     }
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        roundedView.backgroundColor = UIColor.lightGray
         roundedView.layer.cornerRadius = 5.0
     }
-
+    
     // MARK: - GUI Update
-
-    func update(value: NSAttributedString, color: UIColor) {
+    
+    func update(value: NSAttributedString, state: HistoryState) {
         label.attributedText = value
+        let color: UIColor
+        switch state {
+        case .increased:
+            color = UIColor.green
+        case .decreased:
+            color = UIColor.red
+        case .unaffected:
+            color = UIColor.lightGray
+        }
+        
         UIView.animate(withDuration: 0.2) {
             self.roundedView.backgroundColor = color
         }
